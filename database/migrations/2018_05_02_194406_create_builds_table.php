3da29419dc7e512a903eb31778987b558a70e172
Somentus <somentus@gmail.com>
2018-05-06 21:18:16 +0200

    Make migration to add name column to builds table

--- database/migrations/2018_05_02_194406_create_builds_table.php
+++ database/migrations/2018_05_02_194406_create_builds_table.php
@@ -15,7 +15,6 @@ class CreateBuildsTable extends Migration
     {
         Schema::create('builds', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
             $table->unsignedInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->unsignedInteger('processor_id');
