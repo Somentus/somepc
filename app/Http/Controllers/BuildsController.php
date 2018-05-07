f541097d5f5f6fbcbd960fa557656e105e0f3393
Somentus <somentus@gmail.com>
2018-05-07 08:08:38 +0200

    Verify if user is build owner when editing build.

--- app/Http/Controllers/BuildsController.php
+++ app/Http/Controllers/BuildsController.php
@@ -49,31 +49,32 @@ class BuildsController extends Controller
         if(Auth::check()){
             $buildId = request('id');
             $build = Build::find($buildId);
-            if($build) {
-                $build->name = request('name');
-                $build->processor_id = request('processor');
-                $build->motherboard_id = request('motherboard');
-                $build->graphics_id = request('graphics');
-                $build->memory_id = request('memory');
-                $build->storage_id = request('storage');
-                $build->tower_id = request('tower');
-                $build->power_id = request('power');
-                $build->optical_id = request('optical');
-                $build->save();
-            } else {
-                $buildId = Build::create([
-                    'name' => request('name'),
-                    'processor_id' => request('processor'),
-                    'motherboard_id' => request('motherboard'),
-                    'graphics_id' => request('graphics'),
-                    'memory_id' => request('memory'),
-                    'storage_id' => request('storage'),
-                    'tower_id' => request('tower'),
-                    'power_id' => request('power'),
-                    'optical_id' => request('optical'),
-                    'user_id' => Auth::id()
-                ])->id;
-
+            if($build->user_id == Auth::id()) {
+                if($build) {
+                    $build->name = request('name');
+                    $build->processor_id = request('processor');
+                    $build->motherboard_id = request('motherboard');
+                    $build->graphics_id = request('graphics');
+                    $build->memory_id = request('memory');
+                    $build->storage_id = request('storage');
+                    $build->tower_id = request('tower');
+                    $build->power_id = request('power');
+                    $build->optical_id = request('optical');
+                    $build->save();
+                } else {
+                    $buildId = Build::create([
+                        'name' => request('name'),
+                        'processor_id' => request('processor'),
+                        'motherboard_id' => request('motherboard'),
+                        'graphics_id' => request('graphics'),
+                        'memory_id' => request('memory'),
+                        'storage_id' => request('storage'),
+                        'tower_id' => request('tower'),
+                        'power_id' => request('power'),
+                        'optical_id' => request('optical'),
+                        'user_id' => Auth::id()
+                    ])->id;
+                }
             }
 
             return redirect('/builds/'.$buildId);
